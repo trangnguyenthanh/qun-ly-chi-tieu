654ee1dc9b357a39d252d60cf0bee66df305fdf7
trangnguyenthanh <104630137+trangnguyenthanh@users.noreply.github.com>
2024-02-23 10:52:58 +0700

    update

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,20 +1,14 @@
 <template>
   <ion-page>
     <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
+     aa
       </ion-header>
-
+aa
       <ExploreContainer name="Tab 1 page" />
-    </ion-content>
+
   </ion-page>
+
+  
 </template>
 
 <script setup lang="ts">
